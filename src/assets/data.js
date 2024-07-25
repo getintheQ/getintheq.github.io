637775b56bb8c21d00b17cf334dcf55ba145a3d3
khiw nitithadachot <kiw.brw@gmail.com>
2024-07-26 01:07:39 +0700

    v 0.0.1 pre-lanch

---
+++ src/assets/data.js
@@ -0,0 +1,139 @@
+
+
+export const info = [{ text: 'About' }, { text: 'Projects' }, { text: 'Timeline'}  ];
+
+export const technologies = [
+
+   {
+      text: 'JavaScript',
+      logo: 'bx bxl-javascript',
+   },
+   {
+      text: 'PostgreSQL',
+      logo: 'bx bxl-postgresql',
+   },
+   {
+      text: 'AWS',
+      logo: 'bx bxl-aws',
+   },
+   {
+      text: 'Docker',
+      logo: 'bx bxl-docker',
+   },
+   {
+      text: 'Python',
+      logo: 'bx bxl-python',
+   },
+   {
+      text: 'React',
+      logo: 'bx bxl-react',
+   },
+   {
+      text: 'GitHub',
+      logo: 'bx bxl-github',
+   },
+   {
+      text: 'Visual Studio',
+      logo: 'bx bxl-visual-studio',
+   },
+   {
+      text: 'Power BI',
+      logo: 'bx bx-bar-chart-square',
+   },
+   {
+      text: 'Tableau',
+      logo: 'bx bxl-tableau',
+   },
+];
+
+export const projects = [
+   {
+      title: 'GetintheQ',
+      description: 'Developed a web application that allows users to join a virtual queue for a restaurant. The application was built using React, Node.js, PostgreSQL, and AWS. The application was deployed using Docker and AWS.',
+      image : "https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+      skills: [
+         {
+            text: 'React',
+            logo: 'bx bxl-react',
+         },
+         {
+            text: 'Node.js',
+            logo: 'bx bxl-nodejs',
+         },
+         {
+            text: 'PostgreSQL',
+            logo: 'bx bxl-postgresql',
+         },
+         {
+            text: 'AWS',
+            logo: 'bx bxl-aws',
+         },
+         {
+            text: 'Docker',
+            logo: 'bx bxl-docker',
+         },
+      ],
+   },
+];
+
+export const Timeline = [
+   {
+      time: '2021',
+      company: 'GetintheQ',
+      positions: 'Software Engineer',
+      description: 'Developed a web application that allows users to join a virtual queue for a restaurant. The application was built using React, Node.js, PostgreSQL, and AWS. The application was deployed using Docker and AWS.',
+      skills: [
+         { text: 'React', logo: 'bx bxl-react' },
+         { text: 'Node.js', logo: 'bx bxl-nodejs' },
+         { text: 'PostgreSQL', logo: 'bx bxl-postgresql' },
+         { text: 'AWS', logo: 'bx bxl-aws' },
+         { text: 'Docker', logo: 'bx bxl-docker' },
+      ],
+   },
+   {
+      time: '2020',
+      company: 'GetintheQ',
+      positions: 'Software Engineer',
+      description: 'Developed a web application that allows users to join a virtual queue for a restaurant. The application was built using React, Node.js, PostgreSQL, and AWS. The application was deployed using Docker and AWS.',
+      skills: [
+         { text: 'React', logo: 'bx bxl-react' },
+         { text: 'Node.js', logo: 'bx bxl-nodejs' },
+         { text: 'PostgreSQL', logo: 'bx bxl-postgresql' },
+         { text: 'AWS', logo: 'bx bxl-aws' },
+         { text: 'Docker', logo: 'bx bxl-docker' },
+      ],
+   },
+   {
+      time: '2019',
+      company: 'GetintheQ',
+      positions: 'Software Engineer',
+      description: 'Developed a web application that allows users to join a virtual queue for a restaurant. The application was built using React, Node.js, PostgreSQL, and AWS. The application was deployed using Docker and AWS.',
+      skills: [
+         { text: 'React', logo: 'bx bxl-react' },
+         { text: 'Node.js', logo: 'bx bxl-nodejs' },
+         { text: 'PostgreSQL', logo: 'bx bxl-postgresql' },
+         { text: 'AWS', logo: 'bx bxl-aws' },
+         { text: 'Docker', logo: 'bx bxl-docker' },
+      ],
+   },
+   {
+      time: '2018',
+      company: 'GetintheQ',
+      positions: 'Software Engineer',
+      description: 'Developed a web application that allows users to join a virtual queue for a restaurant. The application was built using React, Node.js, PostgreSQL, and AWS. The application was deployed using Docker and AWS.',
+      skills: [
+         { text: 'React', logo: 'bx bxl-react' },
+         { text: 'Node.js', logo: 'bx bxl-nodejs' },
+         { text: 'PostgreSQL', logo: 'bx bxl-postgresql' },
+      ],
+   },
+];
+
+export const buttons = [
+   {
+      github: 'bx bxl-github',
+      demo: 'bx bx-desktop',
+      back: 'bx bxs-left-arrow-circle',
+      hrefgithub: 'https://github.com/getintheq'
+   },
+];
